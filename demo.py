dfac5dfeb3a3b61b9cba68aa88f2cd18d2283bf1
Yalin <13203701351@163.com>
2022-08-05 09:15:07 +0800

    8.5

--- demo.py
+++ demo.py
@@ -1,5 +1,19 @@
-test = True
-if test:
-    print("true")
-else:
-    print("false") 
+paragrarh = '''hello I'm learning rython now''' + r'\n' + '''.
+please text me later'''
+# print(paragrarh)
+str = '1234567'
+print(str[-1::-1])
+# print(str[-1])
+# print(str[0:-1])
+# print(str[0:-2])
+x = 'a'
+y = 'b'
+# print(x,end="")
+# print(y,end="")
+# print()
+# import sys
+# for i in sys.argv:
+#     print(i)
+# print(sys.path)
+# from sys import argv,path
+# print(path)
